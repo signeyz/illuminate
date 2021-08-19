16e6e8117d748440ed2849be848cb1f5875ef4a8
Rob B <rvabcode@gmail.com>
2021-08-19 15:17:44 -0400

    Updates aside nav to jump to about section.

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ closeXBtn.addEventListener('click', function () {
 });
 
 const services = document.querySelector('.services');
-
 services.addEventListener('click', function () {
   sidebar.classList.remove('show-sidebar');
 });
 
 const socials = document.querySelector('.socials');
-
 socials.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
+});
+
+const about = document.querySelector('.about');
+about.addEventListener('click', function () {
   sidebar.classList.remove('show-sidebar');
 });
 
